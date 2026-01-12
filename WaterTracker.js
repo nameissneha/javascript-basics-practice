61b96db4604ba11c7b09190983abc49ea407f787
Sneha Sathe <snehasathe72@gmail.com>
2026-01-12 17:28:28 +0100

    WaterTracker practise set

---
+++ WaterTracker.js
@@ -0,0 +1,16 @@
+//5) Water Tracker Logic (App-style messages)
+
+let totalWater =5000;
+let goalWater = 2000;
+
+if (totalWater >= goalWater) {
+    console.log("Goal Reached"); }
+    else if (totalWater >= goalWater * 0.75) {
+        console.log("Almost there!! Keep going!");}
+
+    else if (totalWater >= goalWater * 0.5){
+        console.log("Halfway there! Good job!");}
+
+    else {
+        console.log("You need to really start drinking water");
+    }
