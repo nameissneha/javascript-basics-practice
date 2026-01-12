60203606d7b82bcca2fa0ebc685832d237919a98
Sneha Sathe <snehasathe72@gmail.com>
2026-01-12 13:35:22 +0100

    student grade system

---
+++ student.js
@@ -0,0 +1,42 @@
+// 1 Student Result System
+
+
+let studentName = "sneha";
+let marks = 85;
+
+let grade ="";
+let status ="";
+
+if (marks >=90){
+    grade = "A";
+    status ="pass";
+}
+else if (marks >= 80){
+    grade = "B";
+    status = "pass";
+
+}
+else if (marks >=70){
+grade = "C";
+status ="pass";
+}
+else if (marks >= 60){
+    garde ="C";
+    status ="Pass";
+}
+
+else if (marks >=50) {
+    grade + "D";
+    status ="pass";
+
+
+}
+else{
+    grade = "F";
+    status ="Fail";
+
+}
+console.log("studentName;", studentName);
+console.log("narks;", marks);
+console.log("grade;", grade);
+console.log("status:", status);
