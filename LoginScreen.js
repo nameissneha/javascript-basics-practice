7197f882fb9b0057ccbce46a4b9b8b67ac030cfd
Sneha Sathe <snehasathe72@gmail.com>
2026-01-12 16:23:56 +0100

    LoginScreen and also role checker

---
+++ LoginScreen.js
@@ -0,0 +1,19 @@
+//3) Login + Role Check
+// You only allow login if username+password match. Then you also check role to show different message.
+
+
+let name ="sneha";
+let password ="12345";
+let role ="Eng";
+
+
+if (name ==="sneha" && password === "122345") {
+    if (role === "Eng") {
+    console.log("welcome Enggineer");
+} else {
+    console.log("Access Denied");
+} 
+}
+else { console.log("invalid login")
+
+}
