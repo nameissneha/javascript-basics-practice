fd4d1b80dea54f0931a1641f20ab6204c632d7a2
Sneha Sathe <snehasathe72@gmail.com>
2026-01-13 13:40:08 +0100

    Huh its day 2--so for 1 hour i will do practicse and then jump to building something impactfull and logc

---
+++ Day-2/LoopPrac.js
@@ -0,0 +1,25 @@
+//1️⃣ for Loop (fixed number of times)
+for (let i = 1; i <= 5; i++) {
+  console.log(i * i);
+}
+
+
+//Example 2: Count from 10 to 1
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
+//2️⃣ while Loop (condition-based)
+//Example 1: Keep dividing by 2 until number becomes 1
+let num = 16;
+
+while (num > 1) {
+  console.log(num);
+  num = num / 2;
+}
+//Example 2: Countdown from 7
+let i = 7;
+
+while (i >= 1) {
+  console.log(i);
+  i--;
+}
